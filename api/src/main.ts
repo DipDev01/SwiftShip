27c8e2996add0129b957f1afcd176ce4e01f44df
Siddhartha51 <thapasiddharth51@gmail.com>
2026-07-12 12:04:01 +0545

    auth page UI

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS for development
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(3000, '0.0.0.0');
+  console.log(`NestJS Backend is running on port 3000`);
 }
 bootstrap();
