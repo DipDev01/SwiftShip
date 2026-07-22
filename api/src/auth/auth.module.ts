c90e40c52610f5df8af537431c49e9655f883354
Jayavir10 <152762763+Jayavir10@users.noreply.github.com>
2026-07-23 00:50:00 +0545

    backend .env example file upload

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { User } from '../users/user.entity';
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: 'swiftship-secret-key',
+      secret: process.env.JWT_SECRET,
       signOptions: {
         expiresIn: '7d',
       },
